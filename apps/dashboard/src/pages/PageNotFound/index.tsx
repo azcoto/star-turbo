f893c137ebb6b0d01dfdc728534e3829af9588a3
Arizal Coto <rizal.zc@gmail.com>
2023-07-07 07:22:18 +0000

    add 404 PageNotFound

---
+++ apps/dashboard/src/pages/PageNotFound/index.tsx
@@ -0,0 +1,9 @@
+function PageNotFound() {
+  return (
+    <main className="flex flex-col h-screen justify-center items-center">
+      <h1 className="text-muted-foreground">404 Page Not Found</h1>
+    </main>
+  );
+}
+
+export default PageNotFound;
--- apps/dashboard/src/router.tsx
+++ apps/dashboard/src/router.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
+import PageNotFound from './pages/PageNotFound';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Dashboard />,
+  },
+  {
+    path: '*',
+    element: <PageNotFound />,
   },
 ]);
 
