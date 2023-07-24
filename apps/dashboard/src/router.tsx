0c28f98ddcefdf6c4ecc05fdf3077065ae086dfb
Arizal Coto <rizal.zc@gmail.com>
2023-07-24 07:14:33 +0000

    fix scroll to top on route change

--- apps/dashboard/src/router.tsx
+++ apps/dashboard/src/router.tsx
@@ -4,11 +4,16 @@ import PageNotFound from './pages/PageNotFound';
 import LoginPage from './pages/Login';
 import PrivateRoute from './PrivateRoute';
 import Home from './pages/Home';
+import ScrollToTop from './ScrollToTop';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <PrivateRoute />,
+    element: (
+      <ScrollToTop>
+        <PrivateRoute />
+      </ScrollToTop>
+    ),
     children: [
       {
         index: true,
