b4239656feae1c6ef0f4cd4f33dbcd536cc75541
Arizal Coto <rizal.zc@gmail.com>
2023-09-25 07:37:40 +0000

    add guard to telemetry endpoint

--- apps/api/src/endpoints/telemetry/index.ts
+++ apps/api/src/endpoints/telemetry/index.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
+import { authGuard } from '@/middlewares/authGuard';
 
 import getTelemetry from './get-telemetry';
 import getRawData from './get-month-raw';
 
 const router = Router();
 
-router.use(getTelemetry);
-router.use(getRawData);
+router.use(authGuard, getTelemetry);
+router.use(authGuard, getRawData);
 export default router;
