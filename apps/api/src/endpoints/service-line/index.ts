8ddd935652cc2f89e7b527625283dc8d0aee71f5
Arizal Coto <rizal.zc@gmail.com>
2023-09-25 07:45:25 +0000

    add authguad to serviceline be

--- apps/api/src/endpoints/service-line/index.ts
+++ apps/api/src/endpoints/service-line/index.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
+import { authGuard } from '@/middlewares/authGuard';
 import getServiceLine from './get-service-line';
 import getUptime from './get-uptime';
 
 const router = Router();
 
-router.use(getServiceLine);
-router.use(getUptime);
+router.use(authGuard, getServiceLine);
+router.use(authGuard, getUptime);
 
 export default router;
